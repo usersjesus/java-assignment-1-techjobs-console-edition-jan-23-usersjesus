a8d659f527d8b0975f6dcd837978fe0b5d5c1607
sjesus <s.jesus10@yahoo.com>
2023-04-16 18:56:07 -0500

    Third Commit

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -79,7 +79,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toUpperCase().contains(value.toUpperCase())) {
                 jobs.add(row);
             }
         }
